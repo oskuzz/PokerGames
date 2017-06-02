1fe8b036fd47a36b57709851d31ba970861109cd
Osku Sirpoma <osku.sirpoma@gmail.com>
2017-06-02 18:32:32 +0300

    Starting to make Turns

---
+++ Poker/src/GUI/PlayerTurn.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GUI;
+
+/**
+ *
+ * @author osku0
+ */
+public class PlayerTurn {
+    public static int Turn1(){
+        return 1;
+    }
+    public static int Turn2(){
+        return 1;
+    }
+    public static int Turn3(){
+        return 1;
+    }
+    public static int Turn4(){
+        return 1;
+    }
+    public static int Turn5(){
+        return 1;
+    }
+}
