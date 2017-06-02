06134ac1ca9e73c2fcd7afa424c0337b76d07e11
Osku Sirpoma <osku.sirpoma@gmail.com>
2017-06-03 00:05:31 +0300

    -

---
+++ Poker/src/GUI/CardWinTest.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GUI;
+
+/**
+ *
+ * @author osku0
+ */
+public class CardWinTest {
+    public static int TableCards3(int Card1, int Card2, int card3){
+        
+        return 0;
+    }
+}
