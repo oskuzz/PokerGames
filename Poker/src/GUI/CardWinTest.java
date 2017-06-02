8c5c55ca3e2d2e89f94c919c6896a924353adad2
Osku Sirpoma <osku.sirpoma@gmail.com>
2017-06-03 00:23:29 +0300

    -

--- Poker/src/GUI/CardWinTest.java
+++ Poker/src/GUI/CardWinTest.java
@@ -10,8 +10,122 @@ package GUI;
  * @author osku0
  */
 public class CardWinTest {
-    public static int TableCards3(int Card1, int Card2, int card3){
-        
+
+    static double Card1;
+    static double Card2;
+    static double Card3;
+    static double Card4;
+    static double Card5;
+    static double Card6;
+    static double Card7;
+    static double Card8;
+    static double Card9;
+    static double Card10;
+
+    public static int TableCards3(double Card1, double Card2, double Card3, double Card4, double Card5) {
+
         return 0;
     }
+
+    public static void HandCards1(double card1, double card2, double card3, double card4) {
+        if(card1 == 0){
+            Card1 = 0;
+        } else if (card2 == 0){
+            Card2 = 0;
+        }else if (card3 == 0){
+            Card3 = 0;
+        }else if (card4 == 0){
+            Card4 = 0;
+        }
+        
+        Card1 = card1;
+        Card2 = card2;
+        Card3 = card3;
+        Card4 = card4;
+    }
+
+    public static void HandCards2(double card1, double card2, double card3, double card4, double card5, double card6) {
+        if(card1 == 0){
+            Card1 = 0;
+        } else if (card2 == 0){
+            Card2 = 0;
+        }else if (card3 == 0){
+            Card3 = 0;
+        }else if (card4 == 0){
+            Card4 = 0;
+        }else if (card5 == 0){
+            Card5 = 0;
+        }else if (card6 == 0){
+            Card6 = 0;
+        }
+        Card1 = card1;
+        Card2 = card2;
+        Card3 = card3;
+        Card4 = card4;
+        Card5 = card5;
+        Card6 = card6;
+    }
+
+    public static void HandCards3(double card1, double card2, double card3, double card4, double card5, double card6, double card7, double card8) {
+        if(card1 == 0){
+            Card1 = 0;
+        } else if (card2 == 0){
+            Card2 = 0;
+        }else if (card3 == 0){
+            Card3 = 0;
+        }else if (card4 == 0){
+            Card4 = 0;
+        }else if (card5 == 0){
+            Card5 = 0;
+        }else if (card6 == 0){
+            Card6 = 0;
+        }else if (card7 == 0){
+            Card7 = 0;
+        }else if (card8 == 0){
+            Card8 = 0;
+        }
+        Card1 = card1;
+        Card2 = card2;
+        Card3 = card3;
+        Card4 = card4;
+        Card5 = card5;
+        Card6 = card6;
+        Card7 = card7;
+        Card8 = card8;
+    }
+
+    public static void HandCards4(double card1, double card2, double card3, double card4, double card5, double card6, double card7, double card8, double card9, double card10) {
+        if(card1 == 0){
+            Card1 = 0;
+        } else if (card2 == 0){
+            Card2 = 0;
+        }else if (card3 == 0){
+            Card3 = 0;
+        }else if (card4 == 0){
+            Card4 = 0;
+        }else if (card5 == 0){
+            Card5 = 0;
+        }else if (card6 == 0){
+            Card6 = 0;
+        }else if (card7 == 0){
+            Card7 = 0;
+        }else if (card8 == 0){
+            Card8 = 0;
+        }else if (card9 == 0){
+            Card9 = 0;
+        }else if (card10 == 0){
+            Card10 = 0;
+        }
+        Card1 = card1;
+        Card2 = card2;
+        Card3 = card3;
+        Card4 = card4;
+        Card5 = card5;
+        Card6 = card6;
+        Card7 = card7;
+        Card8 = card8;
+        Card9 = card9;
+        Card10 = card10;
+    }
+
 }
